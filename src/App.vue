100e0fdef4a851c76002969a6271c21f3bc4f957
mapldx <116471303+mapldx@users.noreply.github.com>
2022-11-09 18:08:03 -0500

    Upload attempt at functional social media meta tags

--- src/App.vue
+++ src/App.vue
@@ -39,18 +39,20 @@ const EMOJIPEDIA = 'https://emojipedia-us.s3.amazonaws.com/social/emoji/sun.png'
 
 export default {
   name: 'App',
-  data() {
-    return {
-      logo: EMOJIPEDIA,
-    };
-  },
   metaInfo: {
     meta: [
-      { name: 'twitter:card', content: 'summary' },
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { property: 'twitter:domain', content: 'yearin.gmgn.today' },
+      { property: 'twitter:url', content: 'https://yearin.gmgn.today/' },
       { name: 'twitter:title', content: '☀️ gm/gn: your 2022 blockchain wrapped' },
       { name: 'twitter:description', content: 'Inspired by Spotify Wrapped, gm/gn is a compilation of data about your cross-chain activity over the past year. Short for good morning/good night, gm/gn is a term to promote positivity and friendship among the Web 3 community.' },
       { name: 'twitter:image', content: 'this.logo' },
     ],
+  },
+  data() {
+    return {
+      logo: EMOJIPEDIA,
+    };
   },
 };
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import VueMeta from 'vue-meta';
 import HomeView from '../views/HomeView.vue';
 
 Vue.use(VueRouter);
+Vue.use(VueMeta);
 
 const routes = [
   {
