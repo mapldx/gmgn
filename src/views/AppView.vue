b6d8b32eaa64606f0f2517b8129672b2d89d9694
mapldx <116471303+mapldx@users.noreply.github.com>
2022-11-09 18:20:04 -0500

    Upload attempt at functional social media meta tags

--- src/views/AppView.vue
+++ src/views/AppView.vue
@@ -8,10 +8,27 @@
 // @ is an alias to /src
 import WrappedCmp from '@/components/WrappedCmp.vue';
 
+const EMOJIPEDIA = 'https://emojipedia-us.s3.amazonaws.com/social/emoji/sun.png';
+
 export default {
   name: 'AppView',
   components: {
     WrappedCmp,
+  },
+  data() {
+    return {
+      logo: EMOJIPEDIA,
+    };
+  },
+  metaInfo: {
+    meta: [
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { property: 'twitter:domain', content: 'yearin.gmgn.today' },
+      { property: 'twitter:url', content: 'https://yearin.gmgn.today/' },
+      { name: 'twitter:title', content: '☀️ gm/gn: your 2022 blockchain wrapped' },
+      { name: 'twitter:description', content: 'Inspired by Spotify Wrapped, gm/gn is a compilation of data about your cross-chain activity over the past year. Short for good morning/good night, gm/gn is a term to promote positivity and friendship among the Web 3 community.' },
+      { name: 'twitter:image', content: 'this.logo' },
+    ],
   },
 };
 </script>
